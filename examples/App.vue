26f5055ff9ed35edb853a2b8c4a51b12b3c7004f
hanbingxu82 <905583741@qq.com>
2021-08-20 09:38:21 +0800

    style:完善 button 样式，以及全局 scss 变量

--- examples/App.vue
+++ examples/App.vue
@@ -1,7 +1,7 @@
 <!--
  * @Author: your name
  * @Date: 2021-08-19 10:12:38
- * @LastEditTime: 2021-08-19 16:39:54
+ * @LastEditTime: 2021-08-20 09:32:13
  * @LastEditors: Please set LastEditors
  * @Description: In User Settings Edit
  * @FilePath: /tinkerbell-ui-next/examples/App.vue
@@ -9,6 +9,76 @@
 
 <template>
   <tb-button type="success" loading>我陪你熬过了苦</tb-button>
+  <br />
+  <br />
+  <tb-button>默认按钮</tb-button>
+  <tb-button type="primary">主要按钮</tb-button>
+  <tb-button type="success">成功按钮</tb-button>
+  <tb-button type="info">信息按钮</tb-button>
+  <tb-button type="warning">警告按钮</tb-button>
+  <tb-button type="danger">危险按钮</tb-button>
+  <br />
+  <br />
+  <tb-button plain>朴素按钮</tb-button>
+  <tb-button type="primary" plain>主要按钮</tb-button>
+  <tb-button type="success" plain>成功按钮</tb-button>
+  <tb-button type="info" plain>信息按钮</tb-button>
+  <tb-button type="warning" plain>警告按钮</tb-button>
+  <tb-button type="danger" plain>危险按钮</tb-button>
+  <br />
+  <br />
+  <tb-button round>圆角按钮</tb-button>
+  <tb-button type="primary" round>主要按钮</tb-button>
+  <tb-button type="success" round>成功按钮</tb-button>
+  <tb-button type="info" round>信息按钮</tb-button>
+  <tb-button type="warning" round>警告按钮</tb-button>
+  <tb-button type="danger" round>危险按钮</tb-button>
+  <br />
+  <br />
+  <tb-button icon="icon-add-bold" circle></tb-button>
+  <tb-button type="primary" icon="icon-add-bold" circle></tb-button>
+  <tb-button type="success" icon="icon-add-bold" circle></tb-button>
+  <tb-button type="info" icon="icon-add-bold" circle></tb-button>
+  <tb-button type="warning" icon="icon-add-bold" circle></tb-button>
+  <tb-button type="danger" icon="icon-add-bold" circle></tb-button>
+
+  <br />
+  <br />
+  <tb-button plain disabled>朴素按钮</tb-button>
+  <tb-button type="primary" plain disabled>主要按钮</tb-button>
+  <tb-button type="success" plain disabled>成功按钮</tb-button>
+  <tb-button type="info" plain disabled>信息按钮</tb-button>
+  <tb-button type="warning" plain disabled>警告按钮</tb-button>
+  <tb-button type="danger" plain disabled>危险按钮</tb-button>
+  <br />
+  <br />
+  <tb-button disabled>默认按钮</tb-button>
+  <tb-button type="primary" disabled>主要按钮</tb-button>
+  <tb-button type="success" disabled>成功按钮</tb-button>
+  <tb-button type="info" disabled>信息按钮</tb-button>
+  <tb-button type="warning" disabled>警告按钮</tb-button>
+  <tb-button type="danger" disabled>危险按钮</tb-button>
+  <br />
+  <br />
+  <tb-button type="text">文字按钮</tb-button>
+  <tb-button type="text" disabled>禁用文字按钮</tb-button>
+  <br />
+  <br />
+  <tb-button type="primary" icon="icon-layers"></tb-button>
+  <tb-button type="primary" icon="icon-lock"></tb-button>
+  <tb-button type="primary" icon="icon-fullscreen-expand"></tb-button>
+  <tb-button type="primary" icon="icon-search">搜索</tb-button>
+  <tb-button type="primary">上传<i class="iconfont icon-rise-filling"></i></tb-button>
+  <br />
+  <br />
+  <tb-button size="medium" round>中等按钮</tb-button>
+  <tb-button size="small" round>小型按钮</tb-button>
+  <tb-button size="mini" round>超小按钮</tb-button>
+  <br />
+  <br />
+  <tb-button size="medium">中等按钮</tb-button>
+  <tb-button size="small">小型按钮</tb-button>
+  <tb-button size="mini">超小按钮</tb-button>
   <div>{{ count }}</div>
   <button @click="countMethod" class="xiao">新增</button>
   <img :src="img + '/image_search/src=http%3A%2F%2Fa1.att.hudong.com%2F57%2F92%2F01300542193590138063924441627.jpg&refer=http%3A%2F%2Fa1.att.hudong.com&app=2002&size=f9999,10000&q=a80&n=0&g=0n&fmt=jpeg?sec=1616717844&t=e7845b5d13738c4df684d05e2d0001fc'" alt="" />
--- packages/index.ts
+++ packages/index.ts
@@ -1,40 +1,37 @@
 /*
  * @Author: your name
  * @Date: 2021-08-19 10:20:07
- * @LastEditTime: 2021-08-19 16:38:58
+ * @LastEditTime: 2021-08-20 09:02:42
  * @LastEditors: Please set LastEditors
  * @Description: In User Settings Edit
  * @FilePath: /tinkerbell-ui-next/packages/index.ts
  */
-// 导入单个组件
+import './assets/iconfont/iconfont.css' //导入icon图标
+import './assets/style/global.scss'
+
+// 导入多个组件
 import tbButton from './Button/index'
 
-
-
-import "./assets/iconfont/iconfont.css"; //导入icon图标
-import "./assets/style/global.scss";
 // 以数组的结构保存组件，便于遍历
-const components = [
-    tbButton
-]
+const components = [tbButton]
 
 // 定义 install 方法
-const install:any = function (Vue:any) {
-    if (install.installed) return
-    install.installed = true
-    // 遍历并注册全局组件
-    components.map(component => {
-        Vue.component(component.name, component)
-    })
+const install: any = function (Vue: any) {
+  if (install.installed) return
+  install.installed = true
+  // 遍历并注册全局组件
+  components.map((component) => {
+    Vue.component(component.name, component)
+  })
 }
 
-if (typeof window !== 'undefined' && (window as any).Vue ) {
-    install((window as any).Vue)
+if (typeof window !== 'undefined' && (window as any).Vue) {
+  install((window as any).Vue)
 }
 
 export default {
-    // 导出的对象必须具备一个 install 方法
-    install,
-    // 组件列表
-    ...components
+  // 导出的对象必须具备一个 install 方法
+  install,
+  // 组件列表
+  ...components,
 }
